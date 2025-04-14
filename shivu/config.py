7f8bc01325cb207c28f8b42565bec5f3b15acfaa
kiwikawai25 <kiwikawai25@gmail.com>
2025-04-15 00:21:14 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,18 +2,18 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
-    sudo_users = "6845325416", "6765826972"
-    GROUP_ID = -1002133191051
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = "-1002133191051"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+    OWNER_ID = "8156600797"
+    sudo_users = "6675050163", "8156600797"
+    GROUP_ID = -1002343417591
+    TOKEN = "7683327822:AAG5cWtPN4zjdY5_BzMwNXDoq69CxDaubcw"
+    mongo_url = "mongodb+srv://GBAN:GBANKACHODA@cluster0gban.ejxkvzo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0gban"
+    PHOTO_URL = ["https://graph.org/file/90344336f0da2961141a8-9129c1a27bb0bf675f.jpg", "https://graph.org/file/3b8e66af1a005897f1ada-e290ec29df788f01cf.jpg"]
+    SUPPORT_CHAT = "https://t.me/Anime_Circle_Club"
+    UPDATE_CHAT = "https://t.me/+vDcCB_w1fxw1YTll"
+    BOT_USERNAME = "Madara_X_Waifu_ProxyBot"
+    CHARA_CHANNEL_ID = "-1002646820042"
+    api_id = 28159105
+    api_hash = "a0936ddf210a7e091e19947c7dc70c91"
 
     
 class Production(Config):
